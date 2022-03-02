3f6fc52d98303b9f64abf71f84ea55ca8e92f6e9
Christian Harjuno <harjuno.tian@gmail.com>
2022-03-02 08:49:00 +0700

    feat: added husky for commit mesasge lintint

--- index.ts
+++ index.ts
@@ -4,11 +4,15 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import compression from "compression";
 import favicon from "serve-favicon";
+import { createClient } from "redis";
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = (process.env.PORT as string) || 3000;
+const REDIS_URL = (process.env.REDIS_URL as string) || undefined;
+
+if (REDIS_URL === undefined) throw new Error("REDIS_URL is not defined");
 
 import versionRouter from "./routes/versionrouter";
 import SuccessReturnStatus from "./assets/classes/200returnstatus";
@@ -32,6 +36,17 @@ app.get("/", (req: Request, res: Response) => {
 	};
 	return new SuccessReturnStatus(res, returnData);
 });
+
+const initializeRedis = async () => {
+	const redisClient = createClient({
+		url: REDIS_URL,
+	});
+	await redisClient.connect();
+	redisClient.on("connect", () => {
+		console.log("Redis connected");
+	});
+	return redisClient;
+};
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
